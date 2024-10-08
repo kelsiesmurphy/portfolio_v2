0ff27f8f5895278f98ddcc434bafafc28e110807
Kelsie Murphy <kelsie@kelsiesmurphy.com>
2024-10-08 01:14:04 +0100

    Fix issues with header nav

---
+++ src/components/react/MobileDrawer.tsx
@@ -0,0 +1,113 @@
+import * as React from "react";
+import { Github, Linkedin, Menu, Minus, Plus } from "lucide-react";
+
+import { Button, buttonVariants } from "@/components/ui/button";
+import {
+  Drawer,
+  DrawerClose,
+  DrawerContent,
+  DrawerDescription,
+  DrawerFooter,
+  DrawerHeader,
+  DrawerTitle,
+  DrawerTrigger,
+} from "@/components/ui/drawer";
+
+export function MobileDrawer() {
+  const [goal, setGoal] = React.useState(350);
+
+  function onClick(adjustment: number) {
+    setGoal(Math.max(200, Math.min(400, goal + adjustment)));
+  }
+
+  return (
+    <Drawer>
+      <DrawerTrigger asChild>
+        <Button variant="ghost" size="icon">
+          <Menu className="h-6 w-6" />
+        </Button>
+      </DrawerTrigger>
+      <DrawerContent>
+        <div className="mx-auto w-full max-w-sm py-12">
+          <div className="space-y-4">
+            <a
+              href="/"
+              className={buttonVariants({ variant: "secondary" }) + " w-full"}
+            >
+              Home
+            </a>
+            <a
+              href="/blog"
+              className={buttonVariants({ variant: "secondary" }) + " w-full"}
+            >
+              Blog
+            </a>
+            <a
+              href="/about"
+              className={buttonVariants({ variant: "secondary" }) + " w-full"}
+            >
+              About Me
+            </a>
+            <a
+              href="https://github.com/kelsiesmurphy"
+              target="_blank"
+              className={
+                buttonVariants({ variant: "outline" }) + " w-full flex gap-2"
+              }
+            >
+              <Github className="h-4 w-4" /> View my GitHub
+            </a>
+            <a
+              href="https://linkedin.com/in/kelsiesmurphy/"
+              target="_blank"
+              className={
+                buttonVariants({ variant: "outline" }) + " w-full flex gap-2"
+              }
+            >
+              <Linkedin className="h-4 w-4" /> Connect on LinkedIn
+            </a>
+            <a
+              href="https://x.com/kelsiesmurphy"
+              target="_blank"
+              className={
+                buttonVariants({ variant: "outline" }) + " w-full flex gap-2"
+              }
+            >
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                width="20"
+                height="20"
+                viewBox="0 0 24 24"
+                fill="none"
+                stroke="currentColor"
+                stroke-width="2"
+                stroke-linecap="round"
+                stroke-linejoin="round"
+              >
+                <polygon
+                  className="st0"
+                  points="21.3,21.1 9.9,2.9 2.7,2.9 14.1,21.1 "
+                ></polygon>
+                <line
+                  className="st0"
+                  x1="2.7"
+                  y1="21.1"
+                  x2="9.9"
+                  y2="14.5"
+                ></line>
+                <line
+                  className="st0"
+                  x1="14.1"
+                  y1="9.5"
+                  x2="21.3"
+                  y2="2.9"
+                ></line>
+              </svg>
+              Follow on X
+            </a>
+          </div>
+        </div>
+      </DrawerContent>
+    </Drawer>
+  );
+}
--- src/components/react/Navigation.tsx
+++ src/components/react/Navigation.tsx
@@ -2,6 +2,8 @@ import { buttonVariants, Button } from "@/components/ui/button";
 import { useState } from "react";
 import { SITE_TITLE } from "@/consts";
 import Socials from "./Socials";
+import { MobileDrawer } from "./MobileDrawer";
+import { ModeToggle } from "./ModeToggle";
 
 export default function Navigation() {
   const [mobileMenuOpen, setMobileMenuOpen] = useState<boolean>(false);
@@ -14,7 +16,11 @@ export default function Navigation() {
       <header className="px-4 flex min-h-28 md:min-h-40 w-full justify-center">
         <nav className="container flex items-center justify-between py-4">
           <div className="flex items-center gap-4">
-            <Button asChild variant="link" className="mx-0 px-0 font-bold text-lg uppercase">
+            <Button
+              asChild
+              variant="link"
+              className="mx-0 px-0 font-bold text-lg uppercase"
+            >
               <a href="/" className="text-inherit">
                 {SITE_TITLE}
               </a>
@@ -28,40 +34,15 @@ export default function Navigation() {
               </a>
             </div>
           </div>
-          <Socials />
-          <Button
-            size="icon"
-            variant="ghost"
-            className="flex flex-col items-center justify-center md:hidden"
-            onClick={() => toggleMobileMenu()}
-          >
-            <span
-              className={`block h-0.5 w-6 rounded-sm bg-primary transition-all duration-100 ease-out ${
-                mobileMenuOpen ? "translate-y-1 rotate-45" : "-translate-y-0.5"
-              }`}
-            ></span>
-            <span
-              className={`my-0.5 block h-0.5 w-6 rounded-sm bg-primary transition-all duration-100 ease-out ${
-                mobileMenuOpen ? "opacity-0" : "opacity-100"
-              }`}
-            ></span>
-            <span
-              className={`block h-0.5 w-6 rounded-sm bg-primary transition-all duration-100 ease-out ${
-                mobileMenuOpen ? "-translate-y-1 -rotate-45" : "translate-y-0.5"
-              }`}
-            ></span>
-          </Button>
-        </nav>
-        {mobileMenuOpen && (
-          <div className="absolute top-0 mt-28 md:mt-40 min-h-screen w-full bg-background p-8">
-            <p>Add Mobile header here</p>
+          <div className="flex gap-4 items-center">
+            <Socials />
+            <ModeToggle />
+            <div className="md:hidden">
+              <MobileDrawer />
+            </div>
           </div>
-        )}
+        </nav>
       </header>
-      {/* <section className="fixed left-0 hidden h-screen w-80 border-r px-6 py-8 lg:flex">
-        <h1>sidebar</h1>
-        <ModeToggle />
-      </section> */}
     </>
   );
 }
